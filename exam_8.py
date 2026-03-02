af1686a92bc00a1033643f4f33e49958739f082f
anicetaseeya <an.bolhovitina@gmail.com>
2026-03-02 16:47:11 +0100

    helloooooo0000

---
+++ exam_8.py
@@ -0,0 +1,9 @@
+# A string is something like a printed map, you cannot remove countries from there , while a list is like a map drawn by pencil (you can remove countries and create your new world order).
+
+printed_map = "Ukraine"
+printed_map = "slay" + printed_map[1:]
+print(printed_map)
+
+pencil_map = ["France", "Germany", "Italy"]
+pencil_map[1] = "Spain"
+print(pencil_map)
